7ebb9cf3bf3c3ef733bea1fb29550f53c7afb89a
gkswotn12345 <84672164+Jaesu26@users.noreply.github.com>
2024-11-06 02:14:39 +0900

    Refactor solver

--- pairrot/solver.py
+++ pairrot/solver.py
@@ -4,7 +4,7 @@ from typing import Literal, Type, TypeAlias
 import numpy as np
 
 from pairrot.hints import Apple, Banana, Carrot, Eggplant, Garlic, Hint, Mushroom
-from pairrot.types import Syllable, Word
+from pairrot.types import Label, Syllable, Word
 from pairrot.utils import get_maybe_possible_words, get_possible_words, is_hangul
 from pairrot.vocab import _VOCAB
 
@@ -23,6 +23,9 @@ class Solver:
     """select 메서드를 통해 정답 후보군을 가장 적게 만드는 단어 하나를 선택.
     feedback 메서드를 통해 단어 힌트를 전달하여 정답 후보군을 갱신.
 
+    Args:
+        word2label: A dictionary for solving the word puzzle.
+
     Examples:
         answer = "정답"
         solver = Solver()
@@ -31,27 +34,35 @@ class Solver:
         ...
     """
 
-    def __init__(self) -> None:
-        self.word2label = _VOCAB
-        self.candidates = get_possible_words(_VOCAB) + get_maybe_possible_words(_VOCAB)
+    def __init__(self, word2label: dict[Word, Label] | None = None) -> None:
+        self.word2label = word2label if word2label is not None else _VOCAB.copy()
+        self.candidates = get_possible_words(self.word2label) + get_maybe_possible_words(self.word2label)
         self.word2scores: defaultdict[Word, list[int]] = defaultdict(list)
         self.word2mean_score: dict[Word, float] = {}
 
     def select(self) -> tuple[Word, float]:
-        """제시할 단어를 선택한다"""
-        self.word2scores.clear()
-        for maybe_answer in self.candidates:
-            self._update_score(maybe_answer)
-        self.word2mean_score = self._compute_word2mean_score()
-        best_word = min(self.word2mean_score, key=self.word2mean_score.get)
-        best_score = self.word2mean_score[best_word]
-        return best_word, best_score
+        """Select best word."""
+        self._setup()
+        self._update_scores()
+        self.word2mean_score = self._reduce_scores_by_word()
+        return self._select_best()
 
-    def _update_score(self, maybe_answer: Word):
-        for pred in self.candidates:
-            first_hint, second_hint = compute_hints(true=maybe_answer, pred=pred)
-            score = len(self._filter_candidates(first_hint, second_hint))
-            self.word2scores[pred].append(score)
+    def _setup(self) -> None:
+        self.word2scores.clear()
+        self.word2mean_score.clear()
+
+    def _update_scores(self) -> None:
+        for true_assumed in self.candidates:
+            for pred in self.candidates:
+                self._update_score(true_assumed, pred)
+
+    def _update_score(self, true: Word, pred: Word) -> None:
+        first_hint, second_hint = compute_hint_pair(true=true, pred=pred)
+        score = self._compute_score(first_hint, second_hint)
+        self.word2scores[pred].append(score)
+
+    def _compute_score(self, first_hint: Hint, second_hint: Hint) -> int:
+        return len(self._filter_candidates(first_hint, second_hint))
 
     def _filter_candidates(self, first_hint: Hint, second_hint: Hint) -> list[Word]:
         return [
@@ -60,10 +71,17 @@ class Solver:
             if first_hint.can_be_answer(word[0], word[1]) and second_hint.can_be_answer(word[1], word[0])
         ]
 
-    def _compute_word2mean_score(self) -> dict[Word, float]:
+    def _reduce_scores_by_word(self, reduction: str = "mean") -> dict[Word, float]:
+        if reduction != "mean":
+            raise ValueError("reduction supports mean only.")
         word2mean_score = {word: np.array(scores, dtype=np.float32).mean() for word, scores in self.word2scores.items()}
         word2mean_score = dict(sorted(word2mean_score.items(), key=lambda item: (item[1], item[0])))
         return word2mean_score
+
+    def _select_best(self) -> tuple[Word, float]:
+        best_word = min(self.word2mean_score, key=self.word2mean_score.get)
+        best_score = self.word2mean_score[best_word]
+        return best_word, best_score
 
     def feedback(self, pred: Word, first_hint_name: HINT_NAME, second_hint_name: HINT_NAME) -> None:
         if len(pred) != 2:
@@ -74,8 +92,12 @@ class Solver:
         second_hint = HINT_BY_NAME[second_hint_name](pred[1])
         self.candidates = self._filter_candidates(first_hint, second_hint)
 
+    def reset(self) -> None:
+        self.candidates = get_possible_words(self.word2label) + get_maybe_possible_words(self.word2label)
+        self._setup()
 
-def compute_hints(*, true: Word, pred: Word) -> tuple[Hint, Hint]:
+
+def compute_hint_pair(*, true: Word, pred: Word) -> tuple[Hint, Hint]:
     first_hint = _compute_hint(true_direct=true[0], true_indirect=true[1], pred=pred[0])
     second_hint = _compute_hint(true_direct=true[1], true_indirect=true[0], pred=pred[1])
     return first_hint, second_hint
