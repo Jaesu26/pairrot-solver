3fd9a0c0bd01490fa63d1a312ca8089d760c1823
gkswotn12345 <84672164+Jaesu26@users.noreply.github.com>
2024-11-08 00:48:29 +0900

    Refactor utils

--- pairrot/utils.py
+++ pairrot/utils.py
@@ -26,12 +26,15 @@ def get_maybe_possible_words(word2label: dict[Word, Label]) -> list[Word]:
 
 @lru_cache(maxsize=NUM_SYLLABLES)
 def decompose_hangul(syllable: Syllable) -> tuple[Jamo, ...]:
-    """한글 자소 전체(초성, 중성, 종성)를 분해하여 반환합니다."""
     if len(syllable) != 1:
-        raise ValueError("Input's length must be 1.")
+        raise ValueError(f"Input's length must be 1. Got: {syllable}")
     if not is_hangul(syllable):
-        raise ValueError("입력 글자가 한글이 아닙니다.")
-    return extract_chosung(syllable), *extract_jungsung(syllable), *extract_jongsung(syllable)
+        raise ValueError(f"Input must be a hangul. Got: {syllable}")
+    return (
+        extract_chosung(syllable),
+        *_decompose_jungsung(extract_jungsung(syllable)),
+        *_decompose_jongsung(*extract_jongsung(syllable)),
+    )
 
 
 def is_hangul(syllable: Syllable) -> bool:
@@ -40,25 +43,28 @@ def is_hangul(syllable: Syllable) -> bool:
 
 @lru_cache(maxsize=NUM_SYLLABLES)
 def extract_chosung(syllable: Syllable) -> Jamo:
-    """한글의 초성을 분해하여 반환합니다."""
     code = ord(syllable) - BASE_CODE
-    chosung_index = code // CHOSUNG_BASE
-    return CHOSUNGS[chosung_index]
+    index = code // CHOSUNG_BASE
+    return CHOSUNGS[index]
 
 
 @lru_cache(maxsize=NUM_SYLLABLES)
-def extract_jungsung(syllable: Syllable) -> tuple[Jamo, ...]:
-    """한글의 중성을 분해하여 개별 모음으로 반환합니다."""
+def extract_jungsung(syllable: Syllable) -> Jamo:
     code = ord(syllable) - BASE_CODE
-    jungsung_index = (code % CHOSUNG_BASE) // JUNGSUNG_BASE
-    jungsung_char = JUNGSUNGS[jungsung_index]
-    return JUNGSUNG_SPLIT_MAP.get(jungsung_char, (jungsung_char,))
+    index = (code % CHOSUNG_BASE) // JUNGSUNG_BASE
+    return JUNGSUNGS[index]
+
+
+def _decompose_jungsung(jungsung: Jamo) -> tuple[Jamo, ...]:
+    return JUNGSUNG_SPLIT_MAP.get(jungsung, (jungsung,))
 
 
 @lru_cache(maxsize=NUM_SYLLABLES)
-def extract_jongsung(syllable: Syllable) -> tuple[Jamo, ...]:
-    """한글의 종성을 분해하여 개별 자음으로 반환합니다."""
+def extract_jongsung(syllable: Syllable) -> Jamo:
     code = ord(syllable) - BASE_CODE
-    jongsung_index = code % JUNGSUNG_BASE
-    jongsung_char = JONGSUNGS[jongsung_index]
-    return JONGSUNG_SPLIT_MAP.get(jongsung_char, (jongsung_char,)) if jongsung_char else ()
+    index = code % JUNGSUNG_BASE
+    return JONGSUNGS[index]
+
+
+def _decompose_jongsung(jongsung: Jamo) -> tuple[Jamo, ...]:
+    return JONGSUNG_SPLIT_MAP.get(jongsung, (jongsung,)) if jongsung else ()
